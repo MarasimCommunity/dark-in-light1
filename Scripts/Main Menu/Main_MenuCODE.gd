ab3a5518e6e6790ddf99ac2c1a9492227b46fe63
MG <mohamedismail.1234112@gmail.com>
2024-12-06 18:54:19 +0200

    MG

--- Scripts/Main Menu/Main_MenuCODE.gd
+++ Scripts/Main Menu/Main_MenuCODE.gd
@@ -1,15 +1,28 @@
 extends Node2D
+var SettingsPressed = false
+@onready var buttons_scene: Node2D = $"BUTTONS SCENE"
+@onready var settings_scene: Node2D = $"SETTINGS SCENE"
+func _physics_process(delta: float) -> void:
+	if SettingsPressed == true :
+		buttons_scene.hide()
+		settings_scene.show()
+	if SettingsPressed == false :
+		buttons_scene.show()
+		settings_scene.hide()
 func _on_play_pressed() -> void:
 	PlayGame()
 
 func _on_settings_pressed() -> void:
-	Settings()
-
+	SettingsPress()
+func _on_back_pressed() -> void:
+	BackSettPress()
 func _on_quit_pressed() -> void:
 	QuitGame()
 func PlayGame() :
-	pass
-func Settings() :
-	pass
+	get_tree().change_scene_to_file("res://Scenes/Open World Lvl 1.tscn")
+func SettingsPress() :
+	SettingsPressed = true
+func BackSettPress() :
+	SettingsPressed = false
 func QuitGame() :
 	get_tree().quit()
