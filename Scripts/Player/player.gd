3b6784ddcda1194aff4e2bd09e3947d132db0d30
MG <mohamedismail.1234112@gmail.com>
2024-12-06 18:06:55 +0200

    MG

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,1 +1,17 @@
 extends CharacterBody2D
+var speed = 100
+var Gravity = 200
+func _physics_process(delta: float) -> void:
+	FunctionsOragnizer()
+func FunctionsOragnizer() :
+	GravityPower()
+	Move()
+func GravityPower() :
+	if not is_on_floor() :
+		position.y += Gravity
+func Move() :
+	if Player.PlayerMove == true :
+		if Input.is_action_pressed("ui_right") :
+			position.x += speed
+		if Input.is_action_pressed("ui_left") :
+			position.x -= speed
